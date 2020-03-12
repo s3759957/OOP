b408ef075049e8ca48575b57b38848544bf2399e
s3759957 <s3759957@rmit.edu.vn>
2020-03-12 15:57:44 +0700

    nine

---
+++ src/wk1/Nine.java
@@ -0,0 +1,11 @@
+package wk1;
+
+public class Nine {
+    public static void main(String[] args) {
+        for (int i = 0; i < 7 ; i++) {
+            for (int j = 1; j < 7; j++ ) {
+
+            }
+        }
+    }
+}
--- src/wk1/number.java
+++ src/wk1/number.java
@@ -2,13 +2,13 @@ package wk1;
 
 public class number {
     public static void main(String[] args) {
-        int [] arr = new int()[100];
-        for (int i =0; i < 100; i ++) {
-            
-        }
-        for (:
-             ) {
+//        int[] arr1 = new int[10];
+//        double a = 1.61 ;
+        System.out.printf("\tMiles\tKilometers\tKilometers\tMiles\n");
+        for (int i = 1; i < 11; i ++) {
 
+            System.out.printf("%5d\t%10.2f\t|\t%10d\t%10.2f\n", i, i*1.61, 15+5*i, (15+5*i)*(1/1.61));
         }
+
     }
 }
