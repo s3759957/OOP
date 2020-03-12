e0b1fc31aa33dd04df7b44ae5e01753ca70d2c78
s3759957 <s3759957@rmit.edu.vn>
2020-03-12 15:59:11 +0700

    nine

--- src/wk1/Nine.java
+++ src/wk1/Nine.java
@@ -4,7 +4,7 @@ public class Nine {
     public static void main(String[] args) {
         for (int i = 0; i < 7 ; i++) {
             for (int j = 1; j < 7; j++ ) {
-
+ergergerger
             }
         }
     }
