8f5a42bb48be07f951d0babab02d6d3ee47f453f
Nguyen <s3759957@maclabsst-base.rmit.edu.vn>
2020-03-12 14:48:21 +0700

    Initial commit

---
+++ src/wk1/helloWorld.java
@@ -0,0 +1,14 @@
+package wk1;
+
+import java.util.Scanner;
+
+public class helloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World !!!");
+        Scanner input = new Scanner(System.in);
+        String name = input.nextLine();
+        System.out.println(name);
+        double a = 5.0 / 2;
+        System.out.println(a);
+    }
+}
---
+++ src/wk1/number.java
@@ -0,0 +1,14 @@
+package wk1;
+
+public class number {
+    public static void main(String[] args) {
+        int [] arr = new int()[100];
+        for (int i =0; i < 100; i ++) {
+            
+        }
+        for (:
+             ) {
+
+        }
+    }
+}
